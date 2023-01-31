1d774a0b1daa37e9336f9505aa9c3266a33c916e
Jay Sun <we311311@gmail.com>
2023-01-31 16:08:37 +0800

    second commit

---
+++ Data_Reward.py
@@ -0,0 +1,191 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, BertTokenizerFast, AutoModel, GPT2Tokenizer, BertTokenizer, BertModel, BertLMHeadModel
+import json
+from torch import nn
+import torch
+import copy
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+import os
+import pandas as pd
+import random
+import numpy as np
+
+class GPT2DataSet(Dataset):
+    def __init__(self, tokenizer = None, max_len = 256, root_path = os.path.join(pwd, 'dataset'), train_path = 'single_emo_T_train.json', val_path = 'single_emo_T_valid.json', test_path = 'single_emo_T_test.json', status = 'train',
+                 shuffle = True) -> None:
+        self.file_path = os.path.join(root_path, (train_path if 'train' in status else (val_path if 'val' in status else test_path)))
+        with open(self.file_path) as f:
+            self.data = json.load(f)
+        self.dataset = []
+        self.tokenizer = BertTokenizer(vocab_file = os.path.join(pwd, 'GPT-2/vocab_small.txt')) if tokenizer is None else tokenizer
+        if  status == 'test':
+            self.data = [i[0] + i[1] for i in self.data]
+        self.max_len = max_len
+        for line in self.data:
+            if '[' not in line or ']' not in line or line.count(']') != 1 or line.count('[') != 1:
+                continue
+            else:
+                self.max_len = max(self.max_len, len(line))
+                self.dataset.append(line)
+        if shuffle:
+            random.shuffle(self.dataset)
+    def __len__(self) -> int:
+        return len(self.dataset)
+    def __getitem__(self, index) -> dict:
+        return Reward.sentence2id(self.dataset[index], self.tokenizer, max_len = self.max_len)
+    @staticmethod
+    def get_toxic_ids_and_non_sense_response(tokenizer, 
+                                         dirty_words = ['幹!','賤貨','米蟲','王八','王八蛋','不要臉','吃屎','敗類','智障','白癡','賤人','下流',
+                                                        '死肥豬','人渣','神經病','賤','尼瑪','無恥','婊','娘炮','魯蛇','廢物', '腦殘'],   
+                                         non_sense_sentences = ['嗯','嗯嗯','隨便啦','隨便啊','都可以','呵呵','哈哈','喔','笑死','是喔','好吧','我不知道',
+                                                                '還好','是啊','對啊','我也是','嘿嘿']):
+        # create toxic word list
+        toxic_ids = []
+        for i in range(len(dirty_words)):
+            ids = tokenizer.encode(dirty_words[i])
+            toxic_ids.append(ids[1:-1])
+        # create non sense sentence list
+        non_sense_ids = []
+        for i in range(len(non_sense_sentences)):
+            ids = tokenizer.encode(non_sense_sentences[i])
+            non_sense_ids.append(ids[1:])
+        return toxic_ids, non_sense_ids
+    
+class Reward(nn.Module):
+    def __init__(self, gpt : GPT2LMHeadModel, question_mark_token, toxic_words : list, non_sense_response : list, eos_token = 0, device = "cpu", gpt_tokenizer = None, bos_token = 101) -> None:
+        super(Reward, self).__init__()
+        self.reward_coefficient = torch.ones(6, device = device) / 6
+        self.gpt = copy.deepcopy(gpt)
+        self.gpt = self.gpt.to(device)
+        self.gpt_tokenizer = BertTokenizer(vocab_file = os.path.join(pwd, 'GPT-2/vocab_small.txt')) if gpt_tokenizer is None else gpt_tokenizer
+        for p in self.gpt.parameters():
+            p.requires_grad = False
+        self.device = device
+        self.eos_token = eos_token
+        self.bos_token = bos_token
+        self.question_mark_token = question_mark_token
+        self.bert_tokenizer = BertTokenizerFast.from_pretrained('bert-base-chinese')
+        self.bert = AutoModel.from_pretrained('ckiplab/bert-base-chinese').to(device)
+        for p in self.bert.parameters():
+            p.requires_grad = False
+        self.toxic_words = toxic_words
+        self.non_sense_response = non_sense_response
+    def update_model(self, gpt):
+        self.gpt = copy.deepcopy(gpt)
+        for p in self.gpt.parameters():
+                p.requires_grad = False
+        self.gpt.eval()
+    def to_device(self, device):
+        self.device = device
+    def update_reward_coefficient(self, gamma):
+        self.reward_coefficient =  copy.deepcopy(gamma)
+    def forward(self, state : list):
+        # state = [batch_size, {prev_utterance, response}] # elements of second dimension are dictionaries composed of previous utterance and subsequent response
+        # prev_utterance = [prev_utterance_seq_length, ]
+        # response = [response_seq_length, ]
+        self.gpt.eval()
+        for idx in range(len(state)):
+            state[idx]['prev_utterance'] = state[idx]['prev_utterance'].to(self.device)
+            state[idx]['response'] = state[idx]['response'].to(self.device)
+        with torch.no_grad():
+            reward = self.reward_coefficient[0] * (self.get_length_reward(state).exp()) # exp for avoiding +- offset each other, if you don't want it, go head delete .exp()
+            reward += self.reward_coefficient[1] * (self.get_question_reward(state).exp())
+            reward += self.reward_coefficient[2] * (self.get_coherence(state).exp())
+            reward += self.reward_coefficient[3] * (self.get_toxicity(state))
+            reward += self.reward_coefficient[4] * (self.get_ease_of_answering(state).exp())
+            reward += self.reward_coefficient[5] * (self.get_reward_semantic_coherence(state).exp())
+
+        return F.normalize(reward, dim = 0)
+    def get_response_prob(self, state, require_grad = False, reverse = False): # reverse means prev_utterance = response, response = prev_utterance
+        state = copy.deepcopy(state)
+        if reverse:
+            state = [{"prev_utterance" : state[idx]['response'], 'response' : state[idx]['prev_utterance']} for idx in range(len(state))]
+        def get_prob():
+            probability = torch.ones((len(state)), device = self.device)
+            for index, state_dict in enumerate(state):
+                utterance, response = state_dict['prev_utterance'].clone().detach(), state_dict['response'].clone().detach()
+                probability[index] *= self.p_seq2seq(utterance, response)
+            return F.normalize(probability, dim = 0)
+        self.gpt.train(require_grad)
+        if not require_grad:
+            with torch.no_grad():
+                return get_prob()
+        else:
+            return get_prob()
+    def p_seq2seq(self, up, down):
+        input_ids = up.clone().detach()
+        probability = 1e20 # refrain from continuously multiplying number 0 < number < 1 such that probability would be too small, which in turn would cause precision problem
+        for i in range(len(down)):
+            logits = self.gpt(input_ids = input_ids)['logits'] 
+            logits = F.softmax(logits, dim = -1)
+            probability *= logits[-1, down[i]]
+            input_ids = torch.cat((input_ids, torch.tensor([down[i]], device = self.device)), dim = -1)
+            if down[i] == self.eos_token:
+                    break
+        return probability
+    def get_length_reward(self, state):
+        return F.normalize(torch.tensor([len(state[idx]['response']) for idx in range(len(state))], device = self.device, dtype = torch.float), dim = 0)
+    def get_question_reward(self, state):
+        return F.normalize(torch.tensor([1 if self.question_mark_token in state[idx]['response'] else 0 for idx in range(len(state))], device = self.device, dtype = torch.float), dim = 0)
+    def transform_from_gpt_to_bert_tokens(self, input_id):
+        original_string = self.gpt_tokenizer.decode(input_id, skip_special_tokens = True).replace(" ", "")
+        return self.bert_tokenizer.encode(original_string, return_tensors = 'pt')[0].to(self.device)
+    def get_coherence(self, state):
+        state = [{"prev_utterance" : self.transform_from_gpt_to_bert_tokens(state[idx]['prev_utterance']), 'response' : self.transform_from_gpt_to_bert_tokens(state[idx]['response'])} for idx in range(len(state))]
+        coherence = torch.zeros((len(state)), device = self.device, dtype = torch.float)
+        cos = nn.CosineSimilarity(dim = -1)
+        for idx in range(len(state)):
+            utterance = self.bert(input_ids = state[idx]['prev_utterance'].unsqueeze(0))['last_hidden_state'][0][0]
+            response = self.bert(input_ids = torch.cat((torch.tensor([self.bos_token], device = self.device), state[idx]['response'])).unsqueeze(0))['last_hidden_state'][0][0]
+            sim = cos(utterance, response)
+            coherence[idx] = sim
+        return F.normalize(coherence, dim = 0)
+    def get_toxicity(self, state):
+        toxicity = []
+        for idx, value in enumerate(state):
+            counter = 0
+            for word in self.toxic_words:
+                if self.x_in_y(word, value['response']):
+                    counter -= 1
+            toxicity.append(counter)
+        return torch.tensor(toxicity, device = self.device)
+    def get_ease_of_answering(self, state):
+        ease_of_answering = torch.zeros((len(state)), device = self.device, dtype = torch.float)
+        for idx in range(len(state)):
+            temp = 0
+            for sentence in self.non_sense_response:
+                temp += self.p_seq2seq(state[idx]['response'], sentence) / len(sentence)
+            temp *= (- 1 / len(self.non_sense_response))
+            ease_of_answering[idx] = temp
+        return F.normalize(ease_of_answering, dim = 0)
+    def get_reward_semantic_coherence(self, state):
+        forward = self.get_response_prob(state)
+        backward = self.get_response_prob(state, reverse = True)
+        return F.normalize(torch.tensor([forward[idx] / len(state[idx]['response']) + backward[idx] / len(state[idx]['prev_utterance']) for idx in range(len(state))], device = self.device, dtype = torch.float), dim = 0)
+    def x_in_y(self, query, base):
+        try:
+            l = len(query)
+        except TypeError:
+            l = 1
+            query = type(base)((query,))
+
+        for i in range(len(base)):
+            if base[i : i+l] == query:
+                return True
+        return False
+    @staticmethod
+    def sentence2id(sentence, tokenizer, emotion_dict = {'其它': 0, '喜歡': 1, '悲傷': 2, '噁心': 3, '憤怒': 4, '開心': 5}, max_len = None, pad_to_max_len = False):
+
+        utterance = sentence[: sentence.index(']') + 1]
+        response = sentence[sentence.index(']') + 1:]
+        prev_utterance = tokenizer.encode(utterance, return_tensors = 'pt')[0]
+        response = tokenizer.encode(response, return_tensors = 'pt')[0][1:]
+        if max_len is not None and pad_to_max_len:
+            padding = max_len - prev_utterance.shape[-1]
+            if padding > 0:
+                prev_utterance = torch.cat((prev_utterance, torch.zeros(padding, dtype = torch.int64) - 1), dim = -1)
+            padding = max_len - response.shape[-1]
+            if padding > 0:
+                response = torch.cat((response, torch.zeros(padding, dtype = torch.int64) - 1), dim = -1)
+            
+        return {'prev_utterance' : prev_utterance, 'response' : response}
---
+++ train.py
@@ -0,0 +1,236 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, BertTokenizerFast, AutoModel, GPT2Tokenizer, BertTokenizer, BertModel, BertLMHeadModel
+import json
+from torch import nn
+import torch
+import copy
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+import os
+import pandas as pd
+import random
+import numpy as np
+from typing import Iterable
+from tqdm import tqdm
+import csv
+import argparse
+from models import *
+from Data_Reward import *
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('Set parameters for training GPT by RL', add_help = False)
+    parser.add_argument('--lr', default = 1e-4, type = float)
+    parser.add_argument('--batch_size', default = 4, type=int)
+    parser.add_argument('--weight_decay', default = 1e-4, type=float)
+    parser.add_argument('--epochs', default = 32, type = int)
+    parser.add_argument('--lr_drop', default = 12, type = int)
+
+    # * Loss coefficients
+    parser.add_argument('--Q_discounter_factor', default = 0.9, type = float)
+
+    parser.add_argument('--kl_control', default = 0.01, type = float,
+                        help = "coefficent of KL loss")
+    parser.add_argument('--gpt_loss_coefficient', default = 0.1, type = float,
+                        help = "coefficent of GPT loss")
+
+    # dataset parameters
+    parser.add_argument('--dataset_folder', default = 'dataset')
+    parser.add_argument('--root_path', default = os.getcwd())
+    parser.add_argument('--gpu', default = 'cuda')
+
+    return parser
+def get_output_dir(pwd):
+    runs_dir = os.path.join(pwd, 'runs')
+    train_dir = os.path.join(runs_dir, 'train')
+    if not os.path.exists(runs_dir):
+        os.mkdir(runs_dir)
+    if not os.path.exists(train_dir):
+        os.mkdir(train_dir)
+    counter = 1
+    exp = f"exp{counter}"
+    while exp in os.listdir(train_dir):
+        counter += 1
+        exp = f"exp{counter}"
+    output_dir = os.path.join(train_dir, exp)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    if not os.path.exists(os.path.join(output_dir, 'models')):
+        os.mkdir(os.path.join(output_dir, 'models'))
+    with open(os.path.join(output_dir, 'log.csv'), 'w', newline = '') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Epoch', 'KLLoss', 'GPTLoss', 'QLoss', 'TOTALLoss'])
+    return output_dir
+def main(args):
+    print(args)
+    lr = args.lr
+    batch_size = args.batch_size
+    weight_decay = args.weight_decay
+    epochs = args.epochs
+    lr_drop = args.lr_drop
+    Q_discounter_factor = args.Q_discounter_factor
+    kl_control = args.kl_control
+    gpt_loss_coefficient = args.gpt_loss_coefficient
+    pwd = args.root_path
+    data_root = os.path.join(pwd, args.dataset_folder)
+    output_dir = get_output_dir(pwd)
+    device = args.gpu
+    gpt2 = GPT2LMHeadModel.from_pretrained(os.path.join(pwd, 'GPT-2/GPT2_finetune_2'))
+    bert = BertModel.from_pretrained('bert-base-chinese')
+    gpt2_tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
+    gpt_wrapper = GPT2Wrapper(gpt2, tokenizer = gpt2_tokenizer, device = device)
+    Q_A = Q(gpt_tokenizer = gpt2_tokenizer, gamma = Q_discounter_factor, bert_name = 'bert-base-chinese')
+    Q_B = Q(gpt_tokenizer = gpt2_tokenizer, gamma = Q_discounter_factor, bert_name = 'bert-base-chinese')
+    toxic_words, non_sense_response = GPT2DataSet.get_toxic_ids_and_non_sense_response(gpt2_tokenizer)
+    R = Reward(gpt = gpt2, question_mark_token = 136, toxic_words = toxic_words, gpt_tokenizer = gpt2_tokenizer,
+                non_sense_response = non_sense_response, eos_token = 102, device = device, bos_token = 101)
+    criterion = nn.MSELoss()
+    gpt_wrapper.to(device)
+    Q_A.to(device)
+    Q_B.to(device)
+    optimizer = torch.optim.Adam([{ 'params': [p for p in gpt_wrapper.parameters() if p.requires_grad]},
+                                { 'params': [p for p in Q_A.parameters() if p.requires_grad]},
+                                { 'params': [p for p in Q_B.parameters() if p.requires_grad]},
+                                ], lr = lr, weight_decay = weight_decay)
+    print('total parameters: ', sum([p.numel() for p in gpt_wrapper.parameters() if p.requires_grad]) + 
+        sum([p.numel() for p in Q_A.parameters() if p.requires_grad]) + 
+        sum([p.numel() for p in Q_B.parameters() if p.requires_grad]))
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, lr_drop)
+    train_dataset = GPT2DataSet(tokenizer = gpt2_tokenizer, max_len = 256, root_path = data_root, status = 'train')
+    test_dataset = GPT2DataSet(tokenizer = gpt2_tokenizer, max_len = 256, root_path = data_root, status = 'test')
+    q_losses = []
+    gpt_losses = []
+    kl_losses = []
+    total_losses = []
+    print('Start training')
+    for epoch in range(epochs):
+        q_loss, kl_loss, gpt_loss, total_loss = train_one_epoch(epoch = epoch, gpt = gpt_wrapper, Q_A = Q_A, Q_B = Q_B,
+                                                                optimizer = optimizer, R = R, dataset = train_dataset, device = device, batch_size = batch_size,
+                                                                max_len = 256, beam = 3, update_time_per_episode = 10, criterion = criterion, 
+                                                                kl_control = kl_control, gpt_loss_coefficient = gpt_loss_coefficient)
+        q_losses.append(q_loss)
+        kl_losses.append(kl_loss)
+        gpt_losses.append(gpt_loss)
+        total_losses.append(total_losses)
+        torch.save({
+                'Q_A': Q_A.state_dict(),
+                'Q_B': Q_B.state_dict(),
+                'GPT': gpt_wrapper.state_dict()
+            }, os.path.join(output_dir, 'models/latest.pth'))
+        if min(q_losses) == q_loss:
+            torch.save({
+                'Q_A': Q_A.state_dict(),
+                'Q_B': Q_B.state_dict(),
+                'GPT': gpt_wrapper.state_dict()
+            }, os.path.join(output_dir, 'models/best_q_loss.pth'))
+        if min(kl_losses) == kl_loss:
+            torch.save({
+                'Q_A': Q_A.state_dict(),
+                'Q_B': Q_B.state_dict(),
+                'GPT': gpt_wrapper.state_dict()
+            }, os.path.join(output_dir, 'models/best_kl_loss.pth'))
+        if min(gpt_losses) == gpt_loss:
+            torch.save({
+                'Q_A': Q_A.state_dict(),
+                'Q_B': Q_B.state_dict(),
+                'GPT': gpt_wrapper.state_dict()
+            }, os.path.join(output_dir, 'models/best_gpt_loss.pth'))
+        if min(total_losses) == total_loss:
+            torch.save({
+                'Q_A': Q_A.state_dict(),
+                'Q_B': Q_B.state_dict(),
+                'GPT': gpt_wrapper.state_dict()
+            }, os.path.join(output_dir, 'models/best_total_loss.pth'))
+        with open(os.path.join(output_dir, 'log.csv'), 'a', newline = '') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([epoch, kl_loss, gpt_loss, q_loss, total_loss])
+            
+            
+            
+def train_one_epoch(epoch: int, gpt: GPT2Wrapper, Q_A: Q, Q_B: Q,  optimizer: torch.optim.Optimizer = None, R: Reward = None,
+                    dataset: Iterable = GPT2DataSet(), device: torch.device = 'cpu', batch_size = 4, max_len = 256, beam = 3, update_time_per_episode = 10, criterion = nn.MSELoss(), kl_control = 0.01,
+                    gpt_loss_coefficient = 0.1):
+    gpt.train()
+    # two Qs for Double DQN
+    Q_A.train()
+    Q_B.train()
+    gpt.to(device)
+    Q_A.to(device)
+    Q_B.to(device)
+    kl_losses = []
+    gpt_losses = []
+    q_losses = []
+    total_losses = []
+    
+
+    with tqdm(total = len(dataset) // batch_size) as t:
+        for step in range(len(dataset) // batch_size):
+            t.set_description(f"Epoch {epoch}")
+
+            prev_utterance = []
+            response = []
+            for mini_step in range(step * batch_size, (step + 1) * batch_size):
+                pair = dataset[mini_step]
+                prev_utterance.append(pair['prev_utterance'].to(device))
+                response.append(pair['prev_utterance'].to(device))
+            utter_length = None
+            generate_time = 0
+            with torch.no_grad(): # generate episode
+                while (utter_length is None or all(utter_length < max_len)) and generate_time < 4:
+                    generate_time += 1
+                    if utter_length is None:
+                        rslt, msk, sco, smlgprbs, utrlen, rsltprvrnce, rsltrspse = gpt(prev_utterance = prev_utterance, response = response, beam = beam, max_len = max_len,
+                                                                device = device)
+                        # soft sampling
+                        previous_Q_distribution= F.softmax((Q_A(prev_utterance = rslt.view(batch_size * beam, -1), response = None, mask = None, bert_tokens = False, max_len = max_len * 2, processed = False) + 
+                        Q_B(prev_utterance = rslt.view(batch_size * beam, -1), response = None, mask = None, bert_tokens = False, max_len = max_len * 2, processed = False)).view(batch_size, -1), dim = -1)
+                        select = torch.multinomial(previous_Q_distribution, 1)
+                        previous_result = rslt.gather(index = select.unsqueeze(-1).expand(-1, -1, max_len * 2), dim = 1).squeeze(1)
+                        results = previous_result.detach().clone()
+                        masks = msk.gather(index = select.unsqueeze(-1).expand(-1, -1, max_len * 2), dim = 1).squeeze(1)
+                        scores = sco.gather(index = select, dim = 1).squeeze(1)
+                        utter_length = utrlen.gather(index = select, dim = 1).squeeze(1)
+                        sum_logprobs = smlgprbs.gather(index = select, dim = 1).squeeze(1)
+                        results_prev_utterance = [rsltprvrnce[i][int(select[i])] for i in range(batch_size)]
+                        results_response = [rsltprvrnce[i][int(select[i])] for i in range(batch_size)]
+                    else:
+                        rslt, msk, sco, smlgprbs, utrlen, rsltprvrnce, rsltrspse = gpt(prev_utterance = previous_result, beam = beam, max_len = max_len,
+                                                            device = device)
+
+                        previous_Q_distribution = F.softmax((Q_A(prev_utterance = rslt.view(batch_size * beam, -1), response = None, mask = None, bert_tokens = False, max_len = max_len * 2, processed = False) + 
+                        Q_B(prev_utterance = rslt.view(batch_size * beam, -1), response = None, mask = None, bert_tokens = False, max_len = max_len * 2, processed = False)).view(batch_size, -1), dim = -1)
+                        select = torch.multinomial(previous_Q_distribution, 1)
+                        previous_result = rslt.gather(index = select.unsqueeze(-1).expand(-1, -1, max_len * 2), dim = 1).squeeze(1)
+                        
+                        results = torch.cat((results, previous_result.detach().clone()), dim = 0)
+                        masks = torch.cat((masks, msk.gather(index = select.unsqueeze(-1).expand(-1, -1, max_len * 2), dim = 1).squeeze(1)), dim = 0)
+                        scores = torch.cat((scores, sco.gather(index = select, dim = 1).squeeze(1)), dim = 0)
+                        utter_length = torch.cat((utter_length, utrlen.gather(index = select, dim = 1).squeeze(1)), dim = 0)
+                        sum_logprobs = torch.cat((sum_logprobs, smlgprbs.gather(index = select, dim = 1).squeeze(1)), dim = 0)
+                        results_prev_utterance.extend([rsltprvrnce[i][int(select[i])] for i in range(batch_size)])
+                        results_response.extend([rsltprvrnce[i][int(select[i])] for i in range(batch_size)])
+                reward = R([{'prev_utterance': utt, 'response': res} for utt, res in zip(results_prev_utterance[batch_size: ], results_response[batch_size: ])])
+                results_Q, mask_Q = Q_A.get_processed(prev_utterance = results, bert_tokens = False, max_len = max_len * 2)
+            for update_time in range(update_time_per_episode):
+                if random.random() >= 0.5: # update Q_A
+                    q_estimate = Q_A.forward(prev_utterance = results_Q[: len(results_Q) - batch_size], response = None, mask = mask_Q[: len(results_Q) - batch_size], bert_tokens = True, max_len = max_len * 2, processed = True)
+                    q_target = reward + Q_A.gamma * Q_B.forward(prev_utterance = results_Q[batch_size: ], response = None, mask = mask_Q[batch_size: ], bert_tokens = True, max_len = max_len * 2, processed = True)
+                else: # update Q_B
+                    q_estimate = Q_B.forward(prev_utterance = results_Q[: len(results_Q) - batch_size], response = None, mask = mask_Q[: len(results_Q) - batch_size], bert_tokens = True, max_len = max_len * 2, processed = True)
+                    q_target = reward + Q_B.gamma * Q_A.forward(prev_utterance = results_Q[batch_size: ], response = None, mask = mask_Q[batch_size: ], bert_tokens = True, max_len = max_len * 2, processed = True)
+                q_loss = criterion(q_estimate, q_target) 
+                kl_loss = - kl_control * torch.log(torch.mean(q_estimate.exp()))
+                probs = gpt.get_prob(results[: len(results_Q) - batch_size], masks[: len(results_Q) - batch_size], results_prev_utterance[: len(results_Q) - batch_size], results_response[: len(results_Q) - batch_size])
+                gpt_loss = torch.sum(probs.detach().clone().exp() * probs * q_target)
+                total_loss = q_loss + kl_control * kl_loss + gpt_loss_coefficient * gpt_loss
+                optimizer.zero_grad()
+                total_loss.backward()
+                optimizer.step()
+                
+                kl_losses.append(kl_loss)
+                gpt_losses.append(gpt_loss)
+                q_losses.append(q_loss)
+                total_losses.append(total_loss)
+                t.set_postfix(klloss = float(kl_losses[-1]), float(gpt_loss = gpt_losses[-1]),
+                          q_loss = float(q_losses[-1]), total_loss = float(total_losses[-1]))
+            t.update(1)
+    return np.mean(q_losses), np.mean(kl_losses), np.mean(gpt_losses), np.mean(total_losses)
+            
